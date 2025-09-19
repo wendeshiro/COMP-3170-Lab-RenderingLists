f7e36f840f2d44e7d4938709c38687ed9a2f3503
wendeshiro <shiro12311@gmail.com>
2025-09-18 17:57:56 -0700

    fix item size

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,7 +4,7 @@ export default function Book({ imgSrc, imgAlt, bookLink, bookTitle, bookPrice })
     return (
         <div className={styles.bookContainer}>
             <a href={bookLink} target="_blank">
-                <img src={imgSrc} alt={imgAlt} />
+                <img className={styles.bookImage} src={imgSrc} alt={imgAlt} />
             </a>
             <a className={styles.bookTitle} href={bookLink} target="_blank">
                 {bookTitle}
